f38e48e822787429fb391d3842a4484b1dd82f62
Aditya Kumar Sah <146981960+adityakumarsah@users.noreply.github.com>
2025-10-05 12:32:02 +0530

    Update api.js

--- src/sevices/api.js
+++ src/sevices/api.js
@@ -1,7 +1,7 @@
 const API_KEY = import.meta.env.VITE_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
 
-console.log(import.meta.env.REACT_APP_TMDB_API_KEY)
+console.log(import.meta.env.VITE_API_KEY)
 
 export const getpopularMovies = async () => {
     const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
